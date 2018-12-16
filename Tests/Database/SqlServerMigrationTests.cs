04fc4e3cbc7d392c52cbfc7393779a43caac9d38
Adrian Iftode <adrian@eplatforms.com>
2018-12-16 22:54:56 +0200

    Use SqlServer Express as default for dev

--- Tests/Database/SqlServerMigrationTests.cs
+++ Tests/Database/SqlServerMigrationTests.cs
@@ -38,7 +38,7 @@ namespace Tests.Database
         {
             //On a CI machine this should come from some configuration value and the db needs to be accessible
             var options = new DbContextOptionsBuilder<InvoicingContext>()
-                .UseSqlServer($@"Server=LAPTOP-LHBJ0CPO\SQLEXPRESS;Database={databaseName};Trusted_Connection=True;")
+                .UseSqlServer($@"Server=(localdb)\MSSQLLocalDB;Database={databaseName};Trusted_Connection=True;")
                 .Options;
             var context = new InvoicingContext(options);
             return context;
