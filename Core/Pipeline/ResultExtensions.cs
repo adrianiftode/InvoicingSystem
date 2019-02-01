ed7d3702cf38f26ac5e3c79077e818a27af949c6
Adrian Iftode <adrian@eplatforms.com>
2019-02-01 23:54:25 +0200

    Rename to ResultExtensions

--- Core/Pipeline/ResultExtensions.cs
+++ Core/Pipeline/ResultExtensions.cs
@@ -2,16 +2,16 @@
 
 namespace Core.Pipeline
 {
-    internal static class ObjectUtils 
+    internal static class ResultExtensions
     {
         public static TResponse ConvertTo<TResponse>(this Result result)
         {
             var itemType = typeof(TResponse).GetGenericArguments().First();
             var genericType = typeof(Result<>).MakeGenericType(itemType);
-            var conversionMethod = genericType.GetMethod("op_Implicit", new[] {typeof(Result)});
+            var conversionMethod = genericType.GetMethod("op_Implicit", new[] { typeof(Result) });
             var @implicit = conversionMethod.Invoke(null, new object[] { result });
 
-            return (TResponse) @implicit;
+            return (TResponse)@implicit;
         }
     }
 }
