9b2cb69e8ee74aa2926e6c901ba058e613144a49
Adrian Iftode <adrian.iftode@gmail.com>
2020-06-12 14:24:13 +0300

    Preview use of ILogger.Moq to test the interactions with ILogger extensions

--- Core/Pipeline/Authorization.cs
+++ Core/Pipeline/Authorization.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,17 +13,20 @@ namespace Core.Pipeline
     public class Authorization<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
         where TRequest : Request
     {
+        private readonly ILogger<Authorization<TRequest, TResponse>> _logger;
         private readonly IAuthorize<TRequest> _authorize;
 
-        public Authorization(IAuthorize<TRequest> authorize)
+        public Authorization(IAuthorize<TRequest> authorize, ILogger<Authorization<TRequest, TResponse>> logger)
         {
             _authorize = authorize;
+            _logger = logger;
         }
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
             RequestHandlerDelegate<TResponse> next)
         {
             if (!await _authorize.Authorize(request))
             {
+                _logger.LogWarning("User is not authorized {user}", request.User?.GetIdentity());
                 return Result.Forbidden.ConvertTo<TResponse>();
             }
 
---
+++ Tests/Core/Pipeline/AuthorizationTests.cs
@@ -0,0 +1,37 @@
+﻿using Core;
+using Core.Pipeline;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Core.Pipeline
+{
+    public class TestRequest : Request
+    {
+        
+    }
+
+    public class AuthorizationTests
+    {
+        [Fact]
+        public async Task When_user_is_not_authorized_a_warning_containing_the_user_identity_is_logged()
+        {
+            // Arrange
+            var authorizeMock = new Mock<IAuthorize<TestRequest>>();
+            authorizeMock.Setup(c => c.Authorize(It.IsAny<TestRequest>())).ReturnsAsync(false);
+            var loggerMock = new Mock<ILogger<Authorization<TestRequest, Result>>>();            
+            var sut = new Authorization<TestRequest, Result>(authorizeMock.Object, loggerMock.Object);
+
+            // Act
+            await sut.Handle(new TestRequest(), default, null);
+
+            // Assert
+            loggerMock.VerifyLog(logger => logger.LogWarning("User is not authorized*"));
+        }
+    }
+}
