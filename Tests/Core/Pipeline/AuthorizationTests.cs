6768c2434c07ea8bbb48cd64e6e0521198357bc9
Adrian Iftode <a.iftode@criteo.com>
2020-11-26 21:00:23 +0100

    Update ILogger.Mock

--- Tests/Core/Pipeline/AuthorizationTests.cs
+++ Tests/Core/Pipeline/AuthorizationTests.cs
@@ -9,20 +9,41 @@ namespace Tests.Core.Pipeline
 {
     public class AuthorizationTests
     {
+        private readonly Mock<IAuthorize<TestRequest>> _authorizeMock;
+        private readonly Mock<ILogger<Authorization<TestRequest, Result>>> _loggerMock;
+        private readonly Authorization<TestRequest, Result> _sut;
+
+        public AuthorizationTests()
+        {
+            _authorizeMock = new Mock<IAuthorize<TestRequest>>();
+            _loggerMock = new Mock<ILogger<Authorization<TestRequest, Result>>>();
+            _sut = new Authorization<TestRequest, Result>(_authorizeMock.Object, _loggerMock.Object);
+        }
+
         [Fact]
         public async Task When_user_is_not_authorized_a_warning_containing_the_user_identity_is_logged()
         {
             // Arrange
-            var authorizeMock = new Mock<IAuthorize<TestRequest>>();
-            authorizeMock.Setup(c => c.Authorize(It.IsAny<TestRequest>())).ReturnsAsync(false);
-            var loggerMock = new Mock<ILogger<Authorization<TestRequest, Result>>>();
-            var sut = new Authorization<TestRequest, Result>(authorizeMock.Object, loggerMock.Object);
+            _authorizeMock.Setup(c => c.Authorize(It.IsAny<TestRequest>())).ReturnsAsync(false);
 
             // Act
-            await sut.Handle(new TestRequest(), default, null);
+            await _sut.Handle(new TestRequest(), default, null);
 
             // Assert
-            loggerMock.VerifyLog(logger => logger.LogWarning("User is not authorized*"));
+            _loggerMock.VerifyLog(logger => logger.LogWarning("User is not authorized*"));
+        }
+
+        [Fact]
+        public async Task When_user_is_authorized_a_warning_containing_the_user_identity_is_not_logged()
+        {
+            // Arrange
+            _authorizeMock.Setup(c => c.Authorize(It.IsAny<TestRequest>())).ReturnsAsync(true);
+
+            // Act
+            await _sut.Handle(new TestRequest(), default, () => Task.FromResult(Result.Success));
+
+            // Assert
+            _loggerMock.VerifyLog(logger => logger.LogWarning("User is not authorized*"), Times.Never);
         }
     }
 }
